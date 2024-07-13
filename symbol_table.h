9d0eb8000e9814084e1609cd08c2b4c58ccba3b7
Guyaloosh <guyaloosh@gmail.com>
2024-07-13 20:21:42 +0300

    add the DockerFile to work with

--- symbol_table.h
+++ symbol_table.h
@@ -7,15 +7,17 @@
 
 #define MAX_SYMBOLS 100 // Maximum number of symbols (functions)
 
-typedef struct {
+typedef struct
+{
     char name[50]; // Function name
     int isStatic;
 } SymbolEntry;
 
 // Symbol table structure
-typedef struct {
+typedef struct
+{
     SymbolEntry entries[MAX_SYMBOLS]; // Array of symbol entries
-    int count; // Number of entries currently in the table
+    int count;                        // Number of entries currently in the table
 } SymbolTable;
 
 #endif
